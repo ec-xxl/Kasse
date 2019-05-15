6d49c1b72a1df8eb1f04d9cbe20fd8fe415bab52
itsme <dennis.klopfer@uni-konstanz.de>
2019-05-15 21:15:43 +0200

    Added "Bestellung Bestätigen" Button. Functionality not 100%

--- Code/Kasse.py
+++ Code/Kasse.py
@@ -1,18 +1,18 @@
 import os
 from functools import partial
-from tkinter import *
+import tkinter as tk
 import sqlite3
 
 
-master = Tk()
-selectedTeam = StringVar()
+master = tk.Tk()
+selectedTeam = tk.StringVar()
 selectedItems = {}
 
 
 def add_player(team: str):
-    popup = Tk()
+    popup = tk.Tk()
     popup.attributes("-topmost", True)
-    e = Entry(popup)
+    e = tk.Entry(popup)
     e.pack()
     e.focus()
     os.system("osk")
@@ -21,10 +21,10 @@ def add_player(team: str):
         player = e.get()  # This is the text you may want to use later
         insert_player = "INSERT INTO players (player_name, team_name) VALUES (?, ?)"    # SQL-String
         runQuery(insert_player, (player, team))     # add to Database
-        listbox_player.insert(END, player)    # add to Listbox
+        listbox_player.insert(tk.END, player)    # add to Listbox
         popup.destroy()
 
-    popButton = Button(popup, text="OK", width=10, command=callback)
+    popButton = tk.Button(popup, text="OK", width=10, command=callback)
     popButton.pack()
 
     popup.mainloop()
@@ -32,30 +32,46 @@ def add_player(team: str):
 
 def get_players(team: str):
     select_player = "SELECT player_name FROM players WHERE team_name = ?"
-    players = runQuery(select_player, (team,), receive=TRUE)
+    players = runQuery(select_player, (team,), receive=tk.TRUE)
     return players
 
 
 def onSelectTeam():
-    listbox_player.delete(0, END)
+    listbox_player.delete(0, tk.END)
     players = get_players(getSelectedTeam())
     for player in players:
-        listbox_player.insert(END, player)
+        listbox_player.insert(tk.END, player)
 
 
 def getSelectedTeam():
     return selectedTeam.get()
 
-def displayOrder(d=selectedItems):
-    listbox_order.delete(0,END)
-    for item in d:
-        listbox_order.insert(END, (item[0], item[1], d[item]))
+def displayOrder():
+    listbox_order.delete(0,tk.END)
+    for item in selectedItems:
+        listbox_order.insert(tk.END, (item[0], item[1], selectedItems[item]))
 
 def onClickItem(name: str, price:float):
     if (name, price) not in selectedItems:
         selectedItems[(name, price)] = 0
     selectedItems[(name, price)] += 1
     displayOrder()
+
+def getSelectedPlayerID():
+    return listbox_player.get(tk.ACTIVE)
+
+def confirmOrder():
+    player_id = getSelectedPlayerID()
+    global selectedItems
+    if player_id:
+        for item in selectedItems:
+            insert_order = "INSERT INTO purchases (player_id, item_name, price, item_quantity, payed) VALUES (?, ?, ?, ?, ?)"
+            data = (player_id, item[0], item[1], selectedItems[item], 0)
+            runQuery(insert_order, data)
+            selectedItems = {}
+            displayOrder()
+
+
 
 
 
@@ -82,21 +98,29 @@ create_table_players = "CREATE TABLE IF NOT EXISTS players(id integer primary ke
 runQuery(create_table_players)
 
 # create Order Table
-create_table_order = "CREATE TABLE IF NOT EXISTS purchases(id integer primary key autoincrement, player_id integer, item_name text, item_quantity integer, price numeric, payed boolean default false)"
+create_table_order = "CREATE TABLE IF NOT EXISTS purchases(id integer primary key autoincrement, player_id integer, item_name text, item_quantity integer, price numeric, payed integer default 0)"
 runQuery(create_table_order)
 
 # get teams from the teams.txt file
 f = open("teams.txt", "r")
 teamList = f.readlines()
 f.close()
+
+
+teamFrame = tk.Frame(master)
+teamFrame.grid(column=0, row=0, rowspan=2)
+
 # Radiobutton for each Team in TeamList
 for team in teamList:
     [name, pos] = team.strip().split(";")
-    b = Radiobutton(master, text=name, command=lambda: onSelectTeam(), variable=selectedTeam, value=name, indicatoron=0)
+    b = tk.Radiobutton(teamFrame, text=name, command=lambda: onSelectTeam(), variable=selectedTeam, value=name, indicatoron=0)
     b.grid(column=int(pos) % 4, row=int(pos) // 4)
 
+itemFrame = tk.Frame(master)
+itemFrame.grid(column=3, row=0)
+
 # Label for Food
-Label(master, text="Essen").grid(column=7, columnspan=2, row=0)
+tk.Label(itemFrame, text="Essen").grid(column=0, row=0)
 
 # Button for each Food in FoodList
 f = open("food.txt", "r")
@@ -104,23 +128,29 @@ foodList = f.readlines()
 f.close()
 for food in foodList:
     [name, price, pos] = food.strip().split(";")
-    b = Button(master, text=name+"\n"+price+"€", command=partial(onClickItem, name, float(price)))
-    b.grid(column=int(pos) % 3 + 7, row=int(pos) // 3 + 1)
+    b = tk.Button(itemFrame, text=name+"\n"+price+"€", command=partial(onClickItem, name, float(price)))
+    b.grid(column=int(pos) % 3, row=int(pos) // 3 + 1)
 
-
+playerFrame = tk.Frame(master)
+playerFrame.grid(column=1,row=0)
 # List of Players in a Team
-listbox_player = Listbox(master)
-listbox_player.grid(column=5, row=0, rowspan=3, columnspan=2)
-
-# List of Items in Order
-listbox_order = Listbox(master)
-listbox_order.grid(column=10, row=0, rowspan=3, columnspan=2)
-
-
-
+listbox_player = tk.Listbox(playerFrame)
+listbox_player.grid(column=0, row=0, columnspan=2)
 # TODO: Add check if a Team is selected. If not create info to select team
-b = Button(master, text="Add Player", command=lambda: add_player(getSelectedTeam()))
-b.grid(row=4, column=5)
-tButton = Button(master, text="Test")
-tButton.grid(column=6, row=4)
+b = tk.Button(playerFrame, text="Add Player", command=lambda: add_player(getSelectedTeam()))
+b.grid(column=0, row=1)
+tButton = tk.Button(playerFrame, text="Test")
+tButton.grid(column=1, row=1)
+
+
+orderFrame = tk.Frame(master)
+orderFrame.grid(column=4, row=0)
+# List of Items in Order
+listbox_order = tk.Listbox(orderFrame)
+listbox_order.grid(column=0, row=0)
+
+b = tk.Button(orderFrame, text="Buchung bestätigen", command=lambda: confirmOrder())
+b.grid(column=0, row=1)
+
+
 master.mainloop()
