1dbb42bb5d64f4291e579990fbd31caf56df91fe
itsme <dennis.klopfer@uni-konstanz.de>
2019-06-29 13:36:38 +0200

    Stornieren als Popup mit Treeview

--- Code/Kasse.py
+++ Code/Kasse.py
@@ -3,9 +3,11 @@ import collections
 import os
 
 import tkinter as tk
+from time import ctime
 from tkinter import ttk
 
 import ttkwidgets as ttkw
+from datetime import time
 from ttkwidgets import ScrolledListbox
 
 import sqlite3
@@ -50,14 +52,14 @@ if platform.system() == "Darwin":
     # root.overrideredirect(1) # Remove shadow & drag bar. Note: Must be used before wm calls otherwise these will be removed.
     root.call("wm", "attributes", ".", "-topmost", "true") # Always keep window on top of others
     # root.geometry("%dx%d+0+0" % (root.winfo_screenwidth(), root.winfo_screenheight()) )
-    root.geometry("%dx%d+0+0" % (1280, 800) )
+    root.geometry("%dx%d+0+0" % (1280, 800))
     # root.call("wm", "attributes", ".", "-fullscreen", "true") # Fullscreen mode
     # root.tk.call("::tk::unsupported::MacWindowStyle", "style", root._w, "plain", "none")
     root.focus_set()
 else:
     # root.call("wm", "attributes", ".", "-topmost", "true") # Always keep window on top of others
-    root.geometry("%dx%d+0+0" % (root.winfo_screenwidth(), root.winfo_screenheight()) )
-    root.call("wm", "attributes", ".", "-fullscreen", "true") # Fullscreen mode
+    root.geometry("%dx%d+0+0" % (1280, 800))
+    # root.call("wm", "attributes", ".", "-fullscreen", "true") # Fullscreen mode
     root.focus_set()
 
 # ---------------------------------------------- #
@@ -216,7 +218,7 @@ def renamePlayer():
             update_player = "UPDATE players SET player_name = ? WHERE id = ?"    # SQL-String
             runQuery(update_player, (player_name, playerID))     # add to Database
             displayPlayers(team)    # add to Listbox
-            popup.destroy()
+            popupPlayerRename.destroy()
 
     if getSelectedPlayerID():
         # create popup window
@@ -265,17 +267,93 @@ def confirmOrder():
         displayOrder()
 
 def stornoOrder():
-    global selectedItems
-    player_id = getSelectedPlayerID()
-    if player_id:
-        for item in selectedItems:
-            insert_order = "INSERT INTO purchases (player_id, item_name, price, item_quantity, is_payed) VALUES (?, ?, ?, ?, ?)"
-            data = (player_id, item[0], -item[1], selectedItems[item], 0)
-            runQuery(insert_order, data)
-        selectedItems = {}
-        selectedTeam.set("")        # Unselect Team
-        displayPlayers("")          # Show empty Player List
-        displayOrder()
+    playerID = getSelectedPlayerID()
+    if playerID:
+        # create window
+        popupRoot = tk.Tk()
+        popupRoot.title("Stornieren")
+        #popupRoot.overrideredirect(1)  # Remove shadow & drag bar. Note: Must be used before wm calls otherwise these will be removed.
+        popupRoot.attributes("-topmost", True)
+        popupRoot.geometry(
+            "%dx%d+%d+%d" % (400, 400, root.winfo_screenwidth() / 2 - 200, root.winfo_screenheight() / 2 - 200))
+        popupRoot.call("wm", "attributes", ".", "-topmost", "true")  # Always keep window on top of others
+        # create frame for treeView and ScrollBar
+        popupWidgetTreeViewFrame = tk.Frame(popupRoot, bg="green")
+        # create treeview
+        popupWidgetTreeView = ttk.Treeview(popupWidgetTreeViewFrame)
+        popupWidgetTreeView["columns"] = ("Preis", "Anzahl", "Gesamt", "Bezahlt")
+        popupWidgetTreeView.heading("#0", text="Bestellung")
+        popupWidgetTreeView.heading("Preis", text="Preis", anchor=tk.W)
+        popupWidgetTreeView.heading("Anzahl", text="Anzahl", anchor=tk.W)
+        popupWidgetTreeView.heading("Gesamt", text="Gesamt")
+        popupWidgetTreeView.heading("Bezahlt", text="Bezahlt")
+        popupWidgetTreeView.column('#0', width=100, stretch=1)
+        popupWidgetTreeView.column('Preis', width=20, stretch=1)
+        popupWidgetTreeView.column('Anzahl', width=10, stretch=1)
+        popupWidgetTreeView.column('Gesamt', width=20, stretch=1)
+        popupWidgetTreeView.column('Bezahlt', width=10, stretch=1)
+        # scrollbar for tree view
+        popupWidgetTreeViewVSB = ttk.Scrollbar(popupWidgetTreeViewFrame, orient="vertical",
+                                               command=popupWidgetTreeView.yview)
+        popupWidgetTreeView.configure(yscrollcommand=popupWidgetTreeViewVSB.set)
+        # sum up purchases
+        select_purchases = "SELECT item_name, price, item_quantity, is_payed, id FROM purchases WHERE player_id = ?"
+        purchases = runQuery(select_purchases, (playerID,), receive=tk.TRUE)
+        total = 0
+        # create an entry in the treeview for every bought item
+        for purchase in purchases:
+            popupWidgetTreeView.insert("", "end", text=purchase[0], values=(
+            "%.2f€" % purchase[1], purchase[2], "%.2f€" % (purchase[1] * purchase[2]), "x" if purchase[3] else ""), tags=purchase[4])
+            total += (purchase[1] * purchase[2])
+
+        # run SQL query
+        def stornoSelection():
+            if popupWidgetTreeView.selection():
+                for item in popupWidgetTreeView.selection():
+                    select_storno = "SELECT player_id, item_name, price, item_quantity, is_payed FROM purchases WHERE id = ?"
+                    storno = runQuery(select_storno, (popupWidgetTreeView.item(item).get("tags")[0],), True)[0]
+
+                    insert_storno = "INSERT INTO purchases (player_id, item_name, price, item_quantity, is_payed) VALUES (?, ?, ?, ?, ?)"
+                    data = (storno[0], storno[1], -storno[2], storno[3], 0)
+                    runQuery(insert_storno, data)
+
+                set_payed = "UPDATE players SET is_payed = 0 WHERE id = ?"
+                runQuery(set_payed, (playerID,))
+                displayPlayers(getSelectedTeam())
+                popupRoot.destroy()
+                popupNotification("Auswahl storniert", 1000, "green")
+            else:
+                popupNotification("Auswahl treffen", 2000, "yellow")
+
+
+        # create labels
+        popupWidgetDescTotal = tk.Label(popupRoot, text="Summe der Einkäufe:", anchor="e", width=1)
+        popupWidgetLabelTotal = tk.Label(popupRoot, text="%.2f€" % total, anchor="w", width=1)
+        popupWidgetLabelExplanation = tk.Label(popupRoot, text="Für eine Mehrfachauswahl >STRG< gedrückt halten", anchor="w", width=1)
+        # create buttons
+        popupWidgetButtonStorno = tk.Button(popupRoot, text="Stornieren", command=stornoSelection, width=1)
+        popupWidgetLabelAbort = tk.Button(popupRoot, text="Abbrechen", command=lambda: popupRoot.destroy(), width=1)
+        # place widgets on grid
+        popupWidgetTreeViewFrame.grid(column=0, row=0, columnspan=2, sticky=tk.NSEW, padx=10, pady=10)
+        popupWidgetDescTotal.grid(column=0, row=1, sticky=tk.EW)
+        popupWidgetLabelTotal.grid(column=1, row=1, sticky=tk.EW)
+        popupWidgetLabelExplanation.grid(column=0, row=2, columnspan=2, sticky=tk.EW)
+        popupWidgetLabelAbort.grid(column=0, row=4, sticky=tk.NSEW, padx=10, pady=10)
+        popupWidgetButtonStorno.grid(column=1, row=4, sticky=tk.NSEW, padx=10, pady=10)
+        popupWidgetTreeView.pack(fill="both", side="left", expand=tk.TRUE)
+        popupWidgetTreeViewVSB.pack(fill="both", side="right")
+        # configure button
+        popupWidgetButtonStorno.config(font=("Verdana", 20))
+        # configure grid
+        popupRoot.columnconfigure(0, weight=1)
+        popupRoot.columnconfigure(1, weight=1)
+        popupRoot.rowconfigure(0, weight=1)
+        popupRoot.rowconfigure(1, weight=0)
+        popupRoot.rowconfigure(2, weight=0)
+        popupRoot.rowconfigure(3, weight=0)
+        popupRoot.rowconfigure(4, weight=0)
+        # mainloop
+        popupRoot.mainloop()
 
 def deleteOrder():
     global selectedItems
@@ -491,10 +569,10 @@ widgets.playerTreeView = ttk.Treeview(frames.playerTreeViewFrame)
 widgets.playerTreeView["columns"]=("one")
 widgets.playerTreeView.heading("#0",  text="Spieler")
 widgets.playerTreeView.heading("one", text="B", anchor=tk.W)
-widgets.playerTreeView.column("#0", width=100, stretch=1)
-widgets.playerTreeView.column("one", width=20, stretch=1)
+# widgets.playerTreeView.column("#0", width=250, stretch=1)
+# widgets.playerTreeView.column("one", width=5, stretch=1)
 
-widgets.playerTreeViewVSB = ttk.Scrollbar(frames.playerTreeViewFrame,orient="vertical",command=widgets.playerTreeView.yview)
+widgets.playerTreeViewVSB = ttk.Scrollbar(frames.playerTreeViewFrame,orient="vertical", command=widgets.playerTreeView.yview)
 widgets.playerTreeView.configure(yscrollcommand=widgets.playerTreeViewVSB.set)
 
 widgets.playerButtonAdd = tk.Button(frames.players, text="Spieler\n hinzufügen", command=addPlayer)
@@ -514,7 +592,7 @@ widgets.playerTreeView.pack(fill="both",side="left",expand=tk.TRUE)
 widgets.playerTreeViewVSB.pack(fill="both",side="right")
 
 frames.players.rowconfigure(0,weight=1)
-frames.players.columnconfigure(0,weight=1)
+frames.players.columnconfigure(0, weight=1)
 
 # ---------------------------------------------- #
 # -------------fill item frame ----------------- #
@@ -574,7 +652,7 @@ for i in range(len(frames.itemcategories)):
 # ---------------------------------------------- #
 
 totalSV = tk.StringVar()
-totalSV.set("0.00 €")
+totalSV.set("Summe: 0.00 €")
 
 frames.totalTreeViewFrame = tk.Frame(frames.total, bg="green")
 
@@ -590,7 +668,7 @@ widgets.totalTreeViewItems.column('Anzahl', width=10, stretch=1)
 widgets.totalTreeViewItems.column('Gesamt', width=20, stretch=1)
 widgets.totalTreeViewItems.bind('<<TreeviewSelect>>', onSelectOrder)
 
-widgets.totalTreeViewItemsVSB = ttk.Scrollbar(frames.totalTreeViewFrame,orient="vertical",command=widgets.totalTreeViewItems.yview)
+widgets.totalTreeViewItemsVSB = ttk.Scrollbar(frames.totalTreeViewFrame,orient="vertical", command=widgets.totalTreeViewItems.yview)
 widgets.totalTreeViewItems.configure(yscrollcommand=widgets.totalTreeViewItemsVSB.set)
 
 widgets.totalLabelSum = tk.Label(frames.total, textvariable=totalSV)
@@ -598,11 +676,11 @@ widgets.totalLabelSum.configure(image=pixel, font=("Verdana", 30), relief="sunke
 
 widgets.totalButtonClear = tk.Button(frames.total, text="Auswahl löschen", command=deleteOrder, highlightbackground="#307F95")
 widgets.totalButtonStorno = tk.Button(frames.total, text="Buchung stornieren", command=stornoOrder, highlightbackground="#307F95")
-widgets.totalButtonConfirm = tk.Button(frames.total, text="Buchung bestätigen", command=confirmOrder, highlightbackground="#307F95")
+widgets.totalButtonConfirm = tk.Button(frames.total, text="Buchung\nbestätigen", command=confirmOrder, highlightbackground="#307F95")
 
 widgets.totalButtonClear.configure(image=pixel, compound="c", font=("Verdana", 15), height=30)
 widgets.totalButtonStorno.configure(image=pixel, compound="c", font=("Verdana", 15), height=30)
-widgets.totalButtonConfirm.configure(image=pixel, compound="c", font=("Verdana", 30), height=70)
+widgets.totalButtonConfirm.configure(image=pixel, compound="c", font=("Verdana", 20), height=70)
 
 frames.totalTreeViewFrame.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 widgets.totalLabelSum.grid(column=0, row=1, sticky=tk.NSEW, padx=10, pady=10)
